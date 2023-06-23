b652f11ef9e875ec73c9deca916fa3aac0e444a1
Yauma9 <pqueijeiro27@gmail.com>
2023-06-22 21:19:20 -0600

    got grading working

--- backend/routes/generar.js
+++ backend/routes/generar.js
@@ -1,33 +1,48 @@
 var express = require('express');
 var plantuml = require('node-plantuml');
-var fs = require('fs');
+var axios = require('axios');
 const db = require('../../config/database');
 
 var app = express();
 
-app.get('/png/:id', function(req, res) {
-    res.set('Content-Type', 'image/png');
+app.get('/png/:id', function (req, res) {
+  res.set('Content-Type', 'image/png');
 
-    // Query the database using the connection pool from database.js
-    db.query('SELECT archivo FROM tareas WHERE id = ?', [req.params.id])
-        .then(function(results) {
-            if (results.length === 0) {
-                // Handle case where no data is found
-                res.status(404).send('Data not found');
-            } else {
-                // Generate PNG image from retrieved PlantUML code
-                var decode = plantuml.decode(results[0].uml);
-                var gen = plantuml.generate({format: 'png'});
+  // Query the database using the connection pool from database.js
+  db.query('SELECT archivo FROM entregas WHERE id = ?', [req.params.id])
+    .then(function (results) {
+      if (results.length === 0) {
+        // Handle case where no data is found
+        res.status(404).send('Data not found');
+      } else {
+        // Get the repository link from the database results
+        var repoLink = results[0].repo_link;
 
-                decode.out.pipe(gen.in);
-                gen.out.pipe(res);
-            }
-        })
-        .catch(function(error) {
+        // Make the GitHub API request
+        axios.get(repoLink)
+          .then(function (response) {
+            // Decode the base64-encoded content of the file
+            var content = Buffer.from(response.data.content, 'base64').toString();
+
+            // Generate PNG image from retrieved PlantUML code
+            var decode = plantuml.decode(content);
+            var gen = plantuml.generate({ format: 'png' });
+
+            decode.out.pipe(gen.in);
+            gen.out.pipe(res);
+          })
+          .catch(function (error) {
             // Handle error
             console.log(error);
             res.status(500).send('Internal server error');
-        });
+          });
+      }
+    })
+    .catch(function (error) {
+      // Handle error
+      console.log(error);
+      res.status(500).send('Internal server error');
+    });
 });
 
 app.listen(3000);
